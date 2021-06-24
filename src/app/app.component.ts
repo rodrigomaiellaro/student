a978c8ff82c89504f4341e95764e091b7cd95870
Rodrigo Maiellaro <rodrigo.maiellaro@gmail.com>
2021-06-24 19:56:50 -0300

    Alterações entre aula2 e aula3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jed-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Aula2';
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ]
+  
+  luke = {name:'Luke', isJedi: true, temple: 'Coruscant'};
+  leia = {name:'Leia', isJedi: false};
+  han = {name:'Han Solo', isJedi: false};
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Student } from './student.model';
 
 @Component({
   selector: 'jed-student',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
+  @Input() student!: Student;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clicked(){
+    console.log(`Student: ${this.student.name}`);
+  }
 }
